92d4fe99b6575f0a1d1e8e8857c3a3f41af6c657
Facundo Veliz <facundoveliz9@gmail.com>
2023-11-24 16:29:00 -0300

    added group_anagrams

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,36 +1,43 @@
-def is_anagram(s, t):
-    if len(s) != len(t):
-        return False
-
-    char_count = {}
-
-    # Increment character count for string s
-    for char in s:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-
-    # Decrement character count for string t
-    for char in t:
-        if char in char_count:
-            char_count[char] -= 1
-        else:
-            return False  # If a character in t is not in s, they can't be anagrams
-
-    # Check if all character counts are zero
-    for count in char_count.values():
-        if count != 0:
+class Solution:
+    @staticmethod
+    def is_anagram(s, t):
+        if len(s) != len(t):
             return False
 
-    return True
+        char_count = {}
 
+        # Increment character count for string s
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
 
-def groupAnagrams(strs):
-    for str1 in strs:
-        for str2 in strs:
-            print(str1, str2)
+        # Decrement character count for string t
+        for char in t:
+            if char in char_count:
+                char_count[char] -= 1
+            else:
+                return False  # If a character in t is not in s, they can't be anagrams
 
+        # Check if all character counts are zero
+        for count in char_count.values():
+            if count != 0:
+                return False
 
-groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
-# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        return True
+
+    @staticmethod
+    def groupAnagrams(strs):
+        final_list = []
+        for str1 in strs:
+            lst = [str1]
+            for str2 in strs:
+                if str1 != str2 and Solution.is_anagram(str1, str2):
+                    lst.append(str2)
+                    strs.remove(str2)
+            final_list.append(lst)
+        print(final_list)
+
+# Example usage:
+Solution.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
